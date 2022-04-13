ab787444faac4c8d07d28f8c42f30e02884bf9d0
Padmavathi1312 <padma.maddukuri1@gmail.com>
2022-04-13 17:20:27 -0500

    updated

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+mongoose = require('mongoose');
+var dolphin = require('./models/dolphinSchema');
+const connectionString = process.env.MONGO_CON
+heroku config:set MONGO_CON='your string'
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dolphinRouter = require('./routes/dolphin');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 var app = express();
 
@@ -27,6 +36,34 @@ app.use('/users', usersRouter);
 app.use('/dolphin', dolphinRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
+
+// We can seed the collection if needed on server start
+async function recreateDB() {
+  // Delete everything
+  await Dolphin.deleteMany();
+  let instance1 = new Dolphin(
+    { name: "Murphy", age: 32, weight: 110 });
+  instance1.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved")
+  });
+
+  let instance2 = new Dolphin(
+    { name: "Paddu", age: 24, weight: 120 });
+  instance2.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Second object saved")
+  });
+
+  let instance3 = new Dolphin(
+    { name: "Chocolate", age: 19, weight: 70 });
+  instance3.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Third object saved")
+  });
+}
+let reseed = true;
+if (reseed) { recreateDB(); }
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
