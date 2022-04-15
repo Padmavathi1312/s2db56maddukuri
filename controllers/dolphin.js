640cbba4a7cd87c46b482898261601fb14beb570
Padmavathi1312 <padma.maddukuri1@gmail.com>
2022-04-14 23:21:21 -0500

    update dolphin.js

--- controllers/dolphin.js
+++ controllers/dolphin.js
@@ -42,9 +42,27 @@ exports.dolphin_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: dolphin delete DELETE ' + req.params.id);
 };
 // Handle Costume update form on PUT.
-exports.dolphin_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: dolphin update PUT' + req.params.id);
+exports.dolphin_update_put = async function (req, res) {
+    //res.send('NOT IMPLEMENTED: Dolphin update PUT' + req.params.id);
+    console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await dolphin.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.name)
+            toUpdate.name = req.body.name;
+        if (req.body.age) toUpdate.age = req.body.age;
+        if (req.body.weight) toUpdate.weight = req.body.weight;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+    }
 };
+
 // VIEWS
 // Handle a show all view
 exports.dolphin_view_all_Page = async function(req, res) {
